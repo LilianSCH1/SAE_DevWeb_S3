791619dfa5f2ca95c4df4b2f9ca4dc7fc0061eec
Valentin088 <valentin.hoja5@etu.univ-lorraine.fr>
2025-11-21 19:10:55 +0100

    connexion à la BDD

--- dbconnect.php
+++ dbconnect.php
@@ -1,20 +1,20 @@
-<?php 
-define('USER',"Lilian"); 
-define('PASSWD',"cmonmdp"); 
-define('SERVER',"localhost"); 
-define('BASE',"mypulse"); 
+<?php
+define('SERVER', "localhost");
+define('BASE', "mypulse");
 
+function dbconnect($user = "root", $passwd = "")
+{
+  $dsn = "mysql:dbname=" . BASE . ";host=" . SERVER;
+  try {
+    $connexion = new PDO($dsn, $user, $passwd);
+    $connexion->exec("set names utf8"); // Support UTF-8
+    return $connexion;
+  } catch (PDOException $e) {
+    printf("Échec de la connexion : %s\n", $e->getMessage());
+    exit();
+  }
+}
 
-function dbconnect(){ 
-  $dsn="mysql:dbname=".BASE.";host=".SERVER; 
-  try{ 
-    $connexion=new PDO($dsn,USER,PASSWD); 
-    $connexion->exec("set names utf8"); //Support utf8
-  } 
-  catch(PDOException $e){ 
-    printf("Échec de la connexion: %s\n", $e->getMessage()); 
-    exit(); 
-  } 
-  return $connexion; 
-} 
-?> 
+// Exemple d’utilisation
+$connexion = dbconnect();
+$connexionLilian = dbconnect("Lilian", "cmonmdp");
