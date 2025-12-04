ad1e2be60911351aa6b4387daf208e48cf6ce9ab
Valentin <valentin.hoja5@etu.univ-lorraine.fr>
2025-12-04 16:36:53 +0100

    suppression

--- login/connexion.php
+++ login/connexion.php
@@ -14,7 +14,6 @@ $errors = [];
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-file_put_contents('../database/debug_post.txt', print_r($_POST, true));
 
 // TRAITEMENT INSCRIPTION
 if (
