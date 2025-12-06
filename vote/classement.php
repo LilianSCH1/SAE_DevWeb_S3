8513241a2c105a135c4a40b96e8a037f30059075
LilianSCH1 <lilian.schmitt1@etu.univ-lorraine.fr>
2025-12-06 10:22:02 +0100

    require modals.js

--- vote/classement.php
+++ vote/classement.php
@@ -80,6 +80,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="../script/script.js"></script>
+    <script src="../script/modals.js"></script>
     
 </body>
 </html>
