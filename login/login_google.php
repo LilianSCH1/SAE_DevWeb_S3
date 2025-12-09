367549ce4a52b2a406912d1e2007b7d5ee659177
Valentin <valentin.hoja5@etu.univ-lorraine.fr>
2025-12-09 09:16:49 +0100

    Modification login Google

--- login/login_google.php
+++ login/login_google.php
@@ -4,30 +4,40 @@ require_once 'vendor/autoload.php';
 
 $client = new Google\Client();
 $client->setClientId($_ENV['GOOGLE_CLIENT_ID']);
-$client->setClientSecret($_$_ENV['GOOGLE_CLIENT_SECRET']);
-$client->setRedirectUri('http://localhost:8080/login_google.php'); // WAMP
-$client->addScope(['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile']);
+$client->setClientSecret($_ENV['GOOGLE_CLIENT_SECRET']);  // ← ICI CORRIGÉ
+$client->setRedirectUri('http://localhost/login_google.php');
+$client->addScope([
+    Google_Service_Oauth2::USERINFO_EMAIL,
+    Google_Service_Oauth2::USERINFO_PROFILE
+]);
 
-$payload = $client->setAccessType('offline')->createAuthUrl();
 if (isset($_GET['code'])) {
-    $accessToken = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-    $client->setAccessToken($accessToken);
-    
-    if ($client->isAccessTokenExpired()) {
-        $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+    $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+    if (!isset($token['error'])) {
+        $client->setAccessToken($token);
+        
+        $service = new Google_Service_Oauth2($client);
+        $userInfo = $service->userinfo->get();
+        
+        $_SESSION['google_user'] = [
+            'id' => $userInfo->id,
+            'email' => $userInfo->email,
+            'name' => $userInfo->name,
+            'picture' => $userInfo->picture
+        ];
+        header('Location: /dashboard.php');
+        exit;
     }
-    
-    $service = new Google_Service_Oauth2($client);
-    $userInfo = $service->userinfo->get();
-    
-    $_SESSION['google_user'] = [
-        'id' => $userInfo->id,
-        'email' => $userInfo->email,
-        'name' => $userInfo->name,
-        'picture' => $userInfo->picture
-    ];
-    header('Location: ../index/index.php');
-    exit;
 }
+
+$authUrl = $client->createAuthUrl();
 ?>
-<a href="<?= $client->createAuthUrl() ?>">Login Google</a>
+<!DOCTYPE html>
+<html>
+<head><title>Google Login</title></head>
+<body>
+    <a href="<?= $authUrl ?>" class="btn btn-primary">
+        <i class="bi bi-google"></i> Se connecter avec Google
+    </a>
+</body>
+</html>
