fdcbb9421779c32409ed025f547b6d16ce5ebd27
Valentin <valentin.hoja5@etu.univ-lorraine.fr>
2025-12-11 11:05:04 +0100

    modif. de la bdd

--- database/mypulse.sql
+++ database/mypulse.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.3
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 08 déc. 2025 à 13:42
--- Version du serveur : 8.3.0
--- Version de PHP : 8.2.18
+-- Généré le : jeu. 11 déc. 2025 à 09:52
+-- Version du serveur : 8.4.7
+-- Version de PHP : 8.5.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -51,21 +51,22 @@ CREATE TABLE IF NOT EXISTS `artiste` (
   `BiographieCourte` text,
   `CheminFichierMP3` varchar(500) NOT NULL,
   `ImageProfil` varchar(500) NOT NULL,
-  `StatusArtiste` enum('en_attente','valide','refusee','classement','archive_suppr', 'archive_classement') NOT NULL DEFAULT 'en_attente',
+  `StatusArtiste` enum('en_attente','valide','refusee','classement','archive') NOT NULL DEFAULT 'en_attente',
   `UserID` int NOT NULL,
   `DateProposition` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `AnneeNaissance` int DEFAULT NULL,
   PRIMARY KEY (`ArtisteID`),
+  UNIQUE KEY `ux_artiste_nom` (`NomArtiste`),
   KEY `UserID` (`UserID`),
   KEY `idx_status` (`StatusArtiste`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `artiste`
 --
 
 INSERT INTO `artiste` (`ArtisteID`, `NomArtiste`, `NomReel`, `BiographieCourte`, `CheminFichierMP3`, `ImageProfil`, `StatusArtiste`, `UserID`, `DateProposition`, `AnneeNaissance`) VALUES
-(11, 'Gims', 'Gandhi Djuna', 'Gims, stylisé GIMS, anciennement Maître Gims, né Gandhi Djuna le 6 mai 1986 à Kinshasa au Zaïre, est un chanteur et rappeur congolais. Il grandit en France et vit principalement entre la France et le Maroc. Il est membre du groupe de hip-hop Sexion d\'assaut.', 'uploads/artistes/sons/Gims_son.mp3', 'uploads/artistes/profil/Gims_profil.webp', 'valide', 6, '2025-12-08 14:24:48', 1986);
+(12, 'SDM', 'Leonard Manzambi', 'SDM, de son vrai nom Leonard Manzambi, né le 28 novembre 1995 à Meudon, est un rappeur français. En 2021, il sort l\'album Ocho, puis, l\'année suivante, Liens du 100 ; ce dernier opus est certifié double disque de platine en treize mois. En 2024, son troisième album, À la vie à la mort, reçoit la même certification en un peu moins de six mois.', 'uploads/artistes/sons/SDM_son.mp3', 'uploads/artistes/profil/SDM_profil.jpg', 'valide', 1, '2025-12-10 20:01:33', 1995);
 
 -- --------------------------------------------------------
 
@@ -106,20 +107,21 @@ CREATE TABLE IF NOT EXISTS `groupe` (
   `BiographieCourte` text,
   `CheminFichierMP3` varchar(500) NOT NULL,
   `ImageGroupe` varchar(500) NOT NULL,
-  `StatusGroupe` enum('en_attente','valide','refusee','classement','archive_suppr', 'archive_classement') NOT NULL DEFAULT 'en_attente',
+  `StatusGroupe` enum('en_attente','valide','refusee','classement','archive') NOT NULL DEFAULT 'en_attente',
   `UserID` int NOT NULL,
   `DateProposition` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`GroupeID`),
+  UNIQUE KEY `ux_groupe_nom` (`NomGroupe`),
   KEY `UserID` (`UserID`),
   KEY `idx_status` (`StatusGroupe`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `groupe`
 --
 
 INSERT INTO `groupe` (`GroupeID`, `NomGroupe`, `AnneeFormation`, `BiographieCourte`, `CheminFichierMP3`, `ImageGroupe`, `StatusGroupe`, `UserID`, `DateProposition`) VALUES
-(5, 'Triangle des Bermudes', '2023', 'Le groupe Triangle des bermudes est composé de MC YOSHI, originaire du quartier des Épinettes situé à Évry-Courcouronnes dans l\'Essonne ; Mauvais djo, d\'origine congolaise, lequel a également grandi à Évry-Courcouronnes ; et Kokosvoice, originaire de Draveil, toujours dans l\'Essonne.', 'uploads/groupes/sons/Triangle_des_Bermudes_son.mp3', 'uploads/groupes/profil/Triangle_des_Bermudes_profil.jpg', 'en_attente', 6, '2025-12-08 14:31:17');
+(9, '2Be3', '1996', '2Be3 est un groupe de pop français, originaire de Longjumeau, dans l\'Essonne. Il est l\'un des premiers boys bands français, formé en 1996, et composé de trois amis d\'enfance originaires de Longjumeau : Filip Nikolic, Adel Kachermi et Frank Delay. Inspiré des boys bands anglo-saxons tels Take That ou Worlds Apart, le groupe a produit trois albums studio ainsi que des compilations. Ils ont vendu cinq millions de disques.', 'uploads/groupes/sons/2Be3_son.mp3', 'uploads/groupes/profil/2Be3_profil.jpg', 'valide', 1, '2025-12-10 20:59:46');
 
 -- --------------------------------------------------------
 
@@ -134,15 +136,33 @@ CREATE TABLE IF NOT EXISTS `musique` (
   `Artiste` varchar(150) NOT NULL,
   `CheminFichierMP3` varchar(500) NOT NULL,
   `ImageCouverture` varchar(500) NOT NULL,
-  `StatusMusique` enum('en_attente','valide','refusee','classement','archive_suppr', 'archive_classement') NOT NULL DEFAULT 'en_attente',
+  `StatusMusique` enum('en_attente','valide','refusee','classement','archive') NOT NULL DEFAULT 'en_attente',
   `UserID` int NOT NULL,
   `DateProposition` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `AnneePublication` int DEFAULT NULL,
   PRIMARY KEY (`MusiqueID`),
+  UNIQUE KEY `ux_musique_chemin` (`CheminFichierMP3`),
+  UNIQUE KEY `ux_musique_titre_artiste` (`Titre`,`Artiste`),
   KEY `UserID` (`UserID`),
   KEY `idx_status` (`StatusMusique`),
   KEY `idx_artiste` (`Artiste`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `musique`
+--
+
+INSERT INTO `musique` (`MusiqueID`, `Titre`, `Artiste`, `CheminFichierMP3`, `ImageCouverture`, `StatusMusique`, `UserID`, `DateProposition`, `AnneePublication`) VALUES
+(22, 'Shamballa', 'Anyme023', 'uploads/musiques/sons/Shamballa_1765386985_musique.mp3', 'uploads/musiques/couvertures/Shamballa_1765386985_couverture.jpg', 'valide', 1, '2025-12-10 18:16:25', 2025),
+(23, 'FE!N', 'Travis Scott', 'uploads/musiques/sons/FEN_1765387072_musique.mp3', 'uploads/musiques/couvertures/FEN_1765387072_couverture.jpg', 'valide', 1, '2025-12-10 18:17:52', 2023),
+(25, 'Goosebumps', 'Travis Scott', 'uploads/musiques/sons/Goosebumps_1765387736_musique.mp3', 'uploads/musiques/couvertures/Goosebumps_1765387736_couverture.jpg', 'valide', 1, '2025-12-10 18:28:56', 2016),
+(27, 'Soleil Bleu', 'Bleu Soleil et Luiza', 'uploads/musiques/sons/Soleil_Bleu_1765387840_musique.mp3', 'uploads/musiques/couvertures/Soleil_Bleu_1765387840_couverture.jpg', 'valide', 1, '2025-12-10 18:30:40', 2025),
+(30, 'Soleil Levant', 'Orelsan et SDM', 'uploads/musiques/sons/Soleil_Levant_1765389622_musique.mp3', 'uploads/musiques/couvertures/Soleil_Levant_1765389622_couverture.jpg', 'valide', 1, '2025-12-10 19:00:22', 2025),
+(31, 'Un monde à l\'autre', 'GIMS, La Mano 1.9 et SCH', 'uploads/musiques/sons/Un_monde__lautre_1765389819_musique.mp3', 'uploads/musiques/couvertures/Un_monde__lautre_1765389819_couverture.jpg', 'valide', 1, '2025-12-10 19:03:39', 2025),
+(33, 'Ailleurs', 'Orelsan', 'uploads/musiques/sons/Ailleurs_1765390046_musique.mp3', 'uploads/musiques/couvertures/Ailleurs_1765390046_couverture.jpg', 'valide', 1, '2025-12-10 19:07:26', 2025),
+(36, 'Die With a Smile', 'Lady Gaga et Bruno Mars', 'uploads/musiques/sons/Die_With_a_Smile_1765394639_musique.mp3', 'uploads/musiques/couvertures/Die_With_a_Smile_1765394639_couverture.jpg', 'valide', 1, '2025-12-10 20:23:59', 2024),
+(37, 'APT.', 'ROSÉ et Bruno Mars', 'uploads/musiques/sons/APT_1765394922_musique.mp3', 'uploads/musiques/couvertures/APT_1765394922_couverture.jpg', 'valide', 1, '2025-12-10 20:28:42', 2024),
+(38, 'BIRDS OF A FEATHER', 'Billie Eilish', 'uploads/musiques/sons/BIRDS_OF_A_FEATHER_1765395240_musique.mp3', 'uploads/musiques/couvertures/BIRDS_OF_A_FEATHER_1765395240_couverture.jpg', 'valide', 1, '2025-12-10 20:34:00', 2024);
 
 -- --------------------------------------------------------
 
@@ -177,19 +197,25 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `UserSurname` varchar(100) NOT NULL,
   `UserMail` varchar(150) NOT NULL,
   `UserPassword` varchar(255) NOT NULL,
-  `Role` enum('admin','certifie','basique') NOT NULL DEFAULT 'basique',
+  `reset_token` varchar(255) DEFAULT NULL,
+  `reset_expire` datetime DEFAULT NULL,
+  `Role` enum('admin','invite','certifie','basique') NOT NULL DEFAULT 'invite',
   `DateInscription` date NOT NULL DEFAULT (curdate()),
+  `Token` varchar(255) NOT NULL,
   PRIMARY KEY (`UserID`),
   UNIQUE KEY `UserMail` (`UserMail`),
+  UNIQUE KEY `Token` (`Token`),
   KEY `idx_role` (`Role`),
   KEY `idx_mail` (`UserMail`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `utilisateur`
 --
 
-INSERT INTO `utilisateur` (`UserID`, `UserPseudo`, `UserName`, `UserSurname`, `UserMail`, `UserPassword`, `Role`, `DateInscription`);
+INSERT INTO `utilisateur` (`UserID`, `UserPseudo`, `UserName`, `UserSurname`, `UserMail`, `UserPassword`, `reset_token`, `reset_expire`, `Role`, `DateInscription`, `Token`) VALUES
+(1, 'Valou', 'Valentin', 'Hoja', 'hoja.valentin@gmail.com', '$2y$12$fU7DDW2FXBUydnINYqQsZe04l1aClyINHCaQP9VgdGy2QLPvciZku', NULL, NULL, 'admin', '2025-12-08', '');
+
 -- --------------------------------------------------------
 
 --
@@ -199,15 +225,14 @@ INSERT INTO `utilisateur` (`UserID`, `UserPseudo`, `UserName`, `UserSurname`, `U
 DROP TABLE IF EXISTS `vote`;
 CREATE TABLE IF NOT EXISTS `vote` (
   `VoteID` int NOT NULL AUTO_INCREMENT,
-  `UserID` int NOT NULL,
   `TypeContenu` enum('musique','chanteur','groupe') NOT NULL,
   `ContenuID` int NOT NULL COMMENT 'ID dans la table correspondante',
   `DateVote` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `ValeurVote` int NOT NULL,
+  `Token` varchar(255) NOT NULL,
   PRIMARY KEY (`VoteID`),
-  UNIQUE KEY `unique_vote` (`UserID`,`TypeContenu`,`ContenuID`),
-  KEY `idx_contenu` (`TypeContenu`,`ContenuID`),
-  KEY `idx_user` (`UserID`)
+  UNIQUE KEY `unique_vote` (`Token`,`TypeContenu`,`ContenuID`),
+  KEY `idx_contenu` (`TypeContenu`,`ContenuID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -231,12 +256,6 @@ ALTER TABLE `groupe`
 --
 ALTER TABLE `musique`
   ADD CONSTRAINT `musique_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `utilisateur` (`UserID`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `vote`
---
-ALTER TABLE `vote`
-  ADD CONSTRAINT `vote_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `utilisateur` (`UserID`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
