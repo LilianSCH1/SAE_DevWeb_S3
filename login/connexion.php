afedd4fd042b4438beee9c07c942e58b71f01541
Valentin <valentin.hoja5@etu.univ-lorraine.fr>
2026-01-07 17:41:49 +0100

    cases cliquables

--- login/connexion.php
+++ login/connexion.php
@@ -181,7 +181,7 @@ if (isset($_SESSION['user_id'])) {
                                         <div class="mb-3 form-check">
                                             <input type="checkbox" class="form-check-input" id="acceptTermsLogin" required>
                                             <label class="form-check-label" for="acceptTermsLogin">
-                                                J'accepte les <a href="#" onclick="event.preventDefault(); event.stopPropagation(); openPageModal('../pages/terms_of_use.php', 'Conditions d\'utilisation')" class="text-decoration-none">conditions générales d'utilisation</a>
+                                                J'accepte les <span onclick="openPageModal('../pages/terms_of_use.php', 'Conditions d\'utilisation')" class="text-decoration-none" style="cursor: pointer;">conditions générales d'utilisation</span>
                                             </label>
                                         </div>
 
@@ -240,7 +240,7 @@ if (isset($_SESSION['user_id'])) {
                                         <div class="mb-3 form-check">
                                             <input type="checkbox" class="form-check-input" id="acceptTerms" required>
                                             <label class="form-check-label" for="acceptTerms">
-                                                J'accepte les <a href="#" class="text-decoration-none">conditions générales d'utilisation</a>
+                                                J'accepte les <a href="#" onclick="event.preventDefault(); event.stopPropagation(); openPageModal('../pages/terms_of_use.php', 'Conditions d\'utilisation')" class="text-decoration-none">conditions générales d'utilisation</a>
                                             </label>
                                         </div>
 
